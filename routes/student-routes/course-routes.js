6b74d8afd295d0c3375757d5a219fb12418eb2a1
Ngan thui <2121050986@student.humg.edu.vn>
2025-08-07 00:52:22 +0700

    fix/free

--- routes/student-routes/course-routes.js
+++ routes/student-routes/course-routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const {
   getStudentViewCourseDetails,
-  getAllStudentViewCourses,
+  getStudentViewCourseList,
   checkCoursePurchaseInfo,
+  enrollFreeCourse,
 } = require("../../controllers/student-controller/course-controller");
+const authenticate = require("../../middleware/auth-middleware");
 const router = express.Router();
 
-router.get("/get", getAllStudentViewCourses);
+router.get("/get", getStudentViewCourseList);
 router.get("/get/details/:id", getStudentViewCourseDetails);
 router.get("/purchase-info/:id/:studentId", checkCoursePurchaseInfo);
+router.post("/enroll-free", authenticate, enrollFreeCourse);
 
 module.exports = router;
